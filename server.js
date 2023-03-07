d34485225d3033cc0e1a2a2c6089de868aca4eec
Andrey <dovban.andreyy@rambler.ru>
2023-03-07 14:11:39 +0300

    работа с сокетами

--- server.js
+++ server.js
@@ -4,10 +4,46 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const ACTIONS = require('./src/socket/actions.ts');
 const PORT = process.env.PORT || 3001;
 
+function getClientRooms() {
+    const { rooms } = io.sockets.adapter;
+
+    return Array.from(rooms.keys());
+}
+
+function shareRoomsInfo() {
+    io.emit(ACTIONS.SHARED_ROOMS, () => {
+        rooms: getClientRooms();
+    });
+}
+
 io.on('connection', (socket) => {
-    console.log('SOCKET CONNECTION');
+    shareRoomsInfo();
+
+    io.on(ACTIONS.JOIN, (config) => {
+        const { room: roomID } = config;
+        const { rooms: joinedRooms } = socket;
+
+        if (Array.from(joinedRooms).includes(roomID)) {
+            return console.warn(`Already joined to ${roomID}`);
+        }
+
+        const clients = Array.from(io.sockets.adapter.rooms.roomID || []);
+
+        clients.forEach((clientID) => {
+            io.to(clientID).emit(ACTIONS.ADD_PEER, {
+                peerID: socket.id,
+                createOffer: false,
+            });
+
+            socket.emit(ACTIONS.ADD_PEER, {
+                perrId: clientID,
+                createOffer: true,
+            });
+        });
+    });
 });
 
 server.listen(PORT, () => {
---
+++ src/socket/actions.ts
@@ -0,0 +1,12 @@
+const ACTIONS = {
+    JOIN: 'join',
+    LEAVE: 'leave',
+    SHARED_ROOMS: 'share-rooms',
+    ADD_PEER: 'add-peer',
+    REALY_SDP: 'realy_sdp',
+    REALY_ICE: 'realy-ice',
+    ICE_CANDIDATE: 'ice-candidate',
+    SESSION_DESCRIPTION: 'session-description',
+};
+
+module.exports = ACTIONS;
